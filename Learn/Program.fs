3972f4597e90027af99d9d28867d1c650237229e
TalhaProgrammer92 <talha.code92@gmail.com>
2025-04-30 20:31:32 +0500

    Added My first F# program

---
+++ Learn/Program.fs
@@ -0,0 +1,24 @@
+﻿open System
+
+//* Function
+let hello() =
+    //? Display Message (prompt)
+    printf "Enter your name: "
+
+    //? Take string input
+    let name = Console.ReadLine()
+
+    //? Display formatted message
+    printfn "Hello, %s!" name
+
+    (*
+    %i - Integer
+    %f - Float
+    %b - 
+    %s - String
+    .*)
+
+//* Function - call
+hello()
+
+Console.ReadKey |> ignore   //! Hold Console Screen
